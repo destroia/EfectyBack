724110daf286ba5cd7b108cb921860905f03b9c0
click <davidstiven123@gmail.com>
2025-04-02 01:23:44 -0500

    parte 2

---
+++ Entity/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        public decimal ValueGain { get; set; }
+        public int TipoDocumento { get; set; }
+        public int EstadoCivil { get; set; }
+    }
+}
